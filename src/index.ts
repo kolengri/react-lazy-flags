f0278e133159fd29c4f2d27f0e919e90335c10b1
Greg Kolens <kolenko@me.com>
2025-12-14 23:54:15 +0000

    refactor: remove default export from Flag component in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 // Main entry point for the package
 export { Flag, type FlagProps } from "./components/Flag";
-export { default } from "./components/Flag";
 export { type FlagCode } from "./components/flags";
